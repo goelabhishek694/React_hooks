4e481a2edddff054f865d031f1d5f3319731edb8
goelabhishek694 <goelabhishek694@gmail.com>
2021-06-27 01:51:28 +0530

    react hooks, useState

---
+++ src/Counter.js
@@ -0,0 +1,24 @@
+import React,{useState}from 'react'
+
+function Counter() {
+
+    const [count,setCount]=useState(0);
+
+    const handleIncrement=()=>{
+        setCount(count+1);
+    }
+
+    const handleDecrement=()=>{
+        setCount(count-1);
+    }
+
+    return (
+        <>
+        <button onClick={handleIncrement}>+</button>
+        <h1>{count}</h1>
+        <button onClick={handleDecrement}>-</button>
+        </>
+    )
+}
+
+export default Counter
